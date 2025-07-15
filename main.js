6c40d8e628ab680ab3ec14b798381bf9ad0569b8
Syconar <urbasekfilip@gmail.com>
2025-07-15 15:28:30 +0200

    Functions and dev env added

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
 
-console.warn("electron os running")
+const isDev = process.env.NODE_ENV !== "development";
+const isMac = process.platform === "darwin";
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Cat Lover",
+        width: isDev ?  1300 : 650,
+        height: 700,
+    });
+
+    // Open devtools if in dev env
+    if(isDev) {
+        mainWindow.webContents.openDevTools();
+    };
+
+    mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
+
+app.whenReady().then(() => {
+  createMainWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+})
