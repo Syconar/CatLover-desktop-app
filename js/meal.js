3661e4edd2931712eaff421ac119b83073546b17
Syconar <urbasekfilip@gmail.com>
2025-07-16 19:32:00 +0200

    Rest of the tools added

--- js/meal.js
+++ js/meal.js
@@ -1,24 +1,26 @@
-let totalSeconds = 30;
+// Set timer of activity
+// let totalSeconds = 30;
 
-function updateTimer() {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
+// function updateTimer() {
+//     const minutes = Math.floor(totalSeconds / 60);
+//     const seconds = totalSeconds % 60;
 
-    document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0') + ':';
-    document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
-}
+//     document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0') + ':';
+//     document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+// }
 
-function startTimer() {
-    updateTimer();
-    const timerInterval = setInterval(() => {
-        totalSeconds--;
-        updateTimer();
+// function startTimer() {
+//     updateTimer();
+//     const timerInterval = setInterval(() => {
+//         totalSeconds--;
+//         updateTimer();
 
-        if (totalSeconds <= 0) {
-            clearInterval(timerInterval);
-            alert("Play time is over")
-        }
-    }, 1000);
-}
+//         if (totalSeconds <= 0) {
+//             clearInterval(timerInterval);
+//             alert("Play time is over")
+//         }
+//     }, 1000);
+// }
 
-window.onload = startTimer;
+// window.onload = startTimer;
+// 
